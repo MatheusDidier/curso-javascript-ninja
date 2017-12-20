c1f0d0164e51ffcd94f6b858e19ba5be4b5f36f4
MatheusDidier <30843684+MatheusDidier@users.noreply.github.com>
2017-12-20 11:28:51 -0200

    Update challenge-16.js

--- challenge-16/challenge-16.js
+++ challenge-16/challenge-16.js
@@ -1,3 +1,5 @@
+(function(){
+  
 /*
 1. Envolva todo o conteúdo desse desafio em uma IIFE.
 2. Adicione a diretiva 'use strict';
@@ -13,8 +15,15 @@ Ex: no caso do nome ser "Fernando", deve mostrar as frases:
 - "e é a 2ª letra do meu nome."
 E assim por diante, até a última.
 */
+  var name = "Matheus";
+  
 console.log( 'As letras do seu nome:' );
 // ?
+  
+    for(var i = 0; i < name.length; i++)
+    {   
+        console.log(name.charAt(i) + " é a " + (i + 1 ) + "º letra do meu nome.");
+    }
 
 /*
 - Declare uma variável chamada `fullName`, que receba seu nome completo,
@@ -30,6 +39,10 @@ console.log para cada formato.
 */
 console.log( '\nNome convertido à partir de um slug:' );
 // ?
+  
+    var fullName = "Matheus-Didier-Fernandes"
+    fullName =  (fullName.split("-").join(" ")).toUpperCase();
+    console.log(fullName);
 
 /*
 - Crie um array com 5 nomes. Reduza esses nomes a uma única string, separando
@@ -41,7 +54,19 @@ O resultado final deve ficar mais ou menos assim:
 5 nomes foi somente uma sugestão ;)
 */
 console.log( '\nMeus amigos:' );
+  
+
 // ?
+  
+      var array = ["Paulo", "Carlos", "Cirius", "Hanna", "Brinda"];
+    var amigos = array.toString();
+    var posicao = amigos.lastIndexOf(",");
+    var ultimoAmigo = amigos.substr(posicao, amigos.length);
+    ultimoAmigo = ultimoAmigo.replace(",", " e ");
+    amigos = amigos.substr(0, posicao);
+    amigos = amigos.concat(ultimoAmigo, " são meus amigos!");
+
+    console.log(amigos);
 
 /*
 Usando o replace(), faça a string "Roberto" virar "Roberta".
@@ -56,6 +81,9 @@ faz a busca do final para o início da string.
 */
 console.log( '\nParte de uma string:' );
 // ?
+  
+      var nome = "Roberto";
+    console.log(nome.replace("Roberto", "Roberta"))
 
 /*
 Declare uma variável chamada `myName`, que receba o seu primeiro nome,
@@ -67,4 +95,12 @@ de qualquer tamanho, escrito de qualquer forma.
 Ex.: Nomes que deveriam funcionar: "Fernando", "RoBertO", "gabriEla", etc.
 */
 console.log( '\nNome com letras intercaladas entre caixa alta e baixa:' );
+  
+      var myName =  "gAbRiElA";
+    var novoNome = "";
+    for(var i = 0; i < myName.length; i++){
+        novoNome += i % 2 == 0 ? myName.charAt(i).toUpperCase() : myName.charAt(i).toLowerCase();
+    }
+    console.log(novoNome);
 // ?
+  }()
