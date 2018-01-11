402eadcea83ba8bd1a1f72219551c9372c16f184
MatheusDidier <30843684+MatheusDidier@users.noreply.github.com>
2018-01-11 15:17:20 -0200

    Update challenge-27.js

--- challenge-27/challenge-27.js
+++ challenge-27/challenge-27.js
@@ -19,3 +19,87 @@ Crie os seguintes métodos para verificação de tipo:
 - isArray, isObject, isFunction, isNumber, isString, isBoolean, isNull.
 O método isNull deve retornar `true` se o valor for null ou undefined.
 */
+
+
+(function(win, doc){
+
+    function DOM(elemento){
+        this.element = doc.querySelectorAll(elemento);
+    }
+    DOM.prototype.on = function on(evento, callback){
+        Array.prototype.forEach.call(this.element, function(item){
+             item.addEventListener(evento, callback, false);
+         });
+    }
+    DOM.prototype.off = function off(evento, callback){
+        Array.prototype.forEach.call(this.element, function(item){
+            item.removeEventListener(evento, callback);
+        });
+    }
+    DOM.prototype.get = function get(){
+        return this.element;
+    }
+    DOM.prototype.forEach = function forEach(callback){
+        Array.prototype.forEach.call(this.element, callback);
+    }
+    DOM.prototype.map = function map(callback){
+        Array.prototype.map.call(this.element, callback);
+    }
+    DOM.prototype.filter = function filter(callback){
+        Array.prototype.filter.call(this.element, callback);
+    }
+    DOM.prototype.reduce = function filter(callback){
+        Array.prototype.reduce.call(this.element, callback);
+    }
+    DOM.prototype.reduceRight = function reduceRight(callback){
+        Array.prototype.reduceRight.call(this.element, callback);
+    }
+    DOM.prototype.every = function every(callback){
+        Array.prototype.every.call(this.element, callback);
+    }
+    DOM.prototype.some = function some(callback){
+        Array.prototype.some.call(this.element, callback);
+    }
+
+    function getTipo(elemento){
+        return Object.prototype.toString.call(elemento);
+    }
+    var $a = new DOM('[data-js="link"]');
+    $a.on('click', function(e) {
+      e.preventDefault();
+      console.log('clicou');
+    });
+
+    DOM.isArray = function(elemento){
+        return getTipo(elemento) == '[object Array]';
+    }
+
+    DOM.isFunction = function(elemento){
+        return getTipo(elemento) == '[object Function]';
+    }
+
+    DOM.isObject = function(elemento){
+        return getTipo(elemento) == '[object Object]';
+    }
+    DOM.isNumber = function(elemento){
+        return getTipo(elemento) == '[object Number]';
+    }
+
+    DOM.isString = function(elemento){
+        return getTipo(elemento) == '[object String]';
+    }
+    DOM.isBoolean = function(elemento){
+        return getTipo(elemento) == '[object Boolean]';
+    }
+    DOM.isNull = function(elemento){
+        return getTipo(elemento) =='[object Null]' || getTipo(elemento) == '[object Undefined]';
+    }
+
+    console.log(DOM.isNull(null));
+
+
+    
+    console.log('Elementos selecionados:', $a.get());
+    console.log('$a é filho de body?', $a.get()[0].parentNode === document.body);
+
+})(window, document);
